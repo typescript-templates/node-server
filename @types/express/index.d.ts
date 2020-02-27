ee5a17db198cd347f5911ed01fd5e0091652929d
Peter Ullrich <dotup.software@gmail.com>
2020-02-27 11:03:33 +0100

    Update 2020-02-27T10:03:27.736Z

--- @types/express/index.d.ts
+++
@@ -1,8 +0,0 @@
-// import { AccountModel } from "../../src/features/account/AccountModel";
-
-// declare module "express-serve-static-core" {
-//   interface Request {
-//     user: AccountModel;
-//     token: any;
-//   }
-// }
