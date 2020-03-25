12de19dd426642df41308042ea085d8f8efe3067
Peter Ullrich <dotup.software@gmail.com>
2020-03-25 06:21:30 +0100

    from express

--- src/errors/DefaultErrorHandler.ts
+++ src/errors/DefaultErrorHandler.ts
@@ -1,6 +1,5 @@
 import { HttpStatusCode } from "@dotup/dotup-ts-types";
-import { Request, Response } from "express-serve-static-core";
-import { NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 
 export const DefaultErrorHandler = (err: Error, req: Request, res: Response, next: NextFunction): void => {
   res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send("Internal server error");
